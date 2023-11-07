d987b0bc32dcd9bb4ccf964817e1edb92a9e54ea
CAOLAN MC DONNELL - STUDENT <g00382160@atu.ie>
2023-11-07 16:20:41 +0000

    LabExamOrderSide

---
+++ src/main/java/com/example/labexamcaolan/OrderService.java
@@ -0,0 +1,2 @@
+package com.example.labexamcaolan;public class OrderService {
+}
---
+++ src/test/java/com/example/labexamcaolan/LabExamCaolanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labexamcaolan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamCaolanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
